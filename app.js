3195446bc5394b75889181e4a603affe6a13338c
r2fresh <r2fresh78@gmail.com>
2013-06-26 06:02:15 +0900

    add bower package

--- app.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var index = fs.readFileSync('index.html');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(index);
-  res.end();
-}).listen(3001);
